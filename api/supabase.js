6669aef8a1c31e16601ebacea29262884cd1e32c
tufutcher <yue.bi@outlook.com>
2026-06-30 02:56:29 +0800

    Update supabase.js with key and url

--- api/supabase.js
+++ api/supabase.js
@@ -1,6 +1,6 @@
 export function initSupabase(){
   return supabase.createClient(
-    "YOUR_URL",
-    "YOUR_KEY"
+    "https://jejjqydkfyacjxspesyh.supabase.co/rest/v1/",
+    "sb_publishable_qRW-sWNFzGuLJIYw4KWHlQ_COyRmj9B"
   );
 }
