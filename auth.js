b02d65d336cdf85854528fa267e6d2959b659f7b
tufutcher <yue.bi@outlook.com>
2026-06-30 02:23:35 +0800

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,31 @@
+import { checkInvite } from "./invite.js";
+
+export async function auth(sb){
+
+  const username = prompt("用户名");
+  const password = prompt("密码");
+  const invite = prompt("邀请码");
+
+  const ok = await checkInvite(sb, invite);
+
+  if(!ok){
+    alert("邀请码错误");
+    return null;
+  }
+
+  const email = username + "@x.com";
+
+  const { data, error } = await sb.auth.signUp({
+    email,
+    password
+  });
+
+  if(error){
+    alert(error.message);
+    return null;
+  }
+
+  const { data: userData } = await sb.auth.getUser();
+
+  return userData?.user || null;
+}
