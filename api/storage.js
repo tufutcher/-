4ee942b388aad84f4fc89155f43c0fd7668b4aff
tufutcher <yue.bi@outlook.com>
2026-06-30 15:07:56 +0800

    Update storage.js

--- api/storage.js
+++ api/storage.js
@@ -1,8 +1,9 @@
-export async function uploadImage(sb,file,path){
-
-  await sb.storage.from("art").upload(path,file);
-
+export async function uploadImage(sb, file, path){
+  const { error } = await sb.storage.from("art").upload(path, file);
+  if(error){
+    alert("图片上传失败：" + error.message);
+    return null;
+  }
   const { data } = sb.storage.from("art").getPublicUrl(path);
-
   return data.publicUrl;
 }
