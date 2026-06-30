d20fddd35c1804f3f6a8abd195ee3b705db53b8b
tufutcher <yue.bi@outlook.com>
2026-06-30 15:10:50 +0800

    Update wall.js with claude

--- modules/wall.js
+++ modules/wall.js
@@ -1,22 +1,23 @@
 export function renderWall(items){
-
   return `
     <div>
       <div class="topbar">
         <button onclick="switchView('wall')">墙</button>
         <button onclick="switchView('me')">我的</button>
       </div>
-
-      ${items.map(i=>`
-        <div class="card">
-          <b>${i.username}</b>
-          <div>${i.note || ""}</div>
-
-          ${i.checkin_images?.map(img=>`
-            <img src="${img.image_url}" />
-          `).join("")}
-        </div>
-      `).join("")}
+      ${
+        items.length
+          ? items.map(i => `
+            <div class="card">
+              <b>${i.username || "匿名"}</b>
+              <div>${i.note || ""}</div>
+              ${(i.checkin_images || []).map(img => `
+                <img src="${img.image_url}" />
+              `).join("")}
+            </div>
+          `).join("")
+          : `<div class="card">还没有人打卡，点右下角"＋"第一个来！</div>`
+      }
     </div>
   `;
 }
