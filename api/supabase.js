b975a02c979413219346990e52c36d416eead15b
tufutcher <yue.bi@outlook.com>
2026-06-30 15:35:40 +0800

    Update supabase.js

--- api/supabase.js
+++ api/supabase.js
@@ -1,6 +1,6 @@
 export function initSupabase(){
   return supabase.createClient(
-    "https://jejjqydkfyacjxspesyh.supabase.co/rest/v1/",
+    "https://jejjqydkfyacjxspesyh.supabase.co",
     "sb_publishable_qRW-sWNFzGuLJIYw4KWHlQ_COyRmj9B"
   );
 }
